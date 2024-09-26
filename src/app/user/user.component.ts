8047d5bdbccdfb489df64c483642e1a6534a996f
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-26 16:32:16 +0200

    added output of names, mails and citys

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,7 +4,9 @@ import { MatIcon } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddUserComponent } from './dialogs/add-user/add-user.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
+import { ContactInterface } from '../interfaces/contact.interface';
 
 
 
@@ -19,9 +21,48 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class UserComponent {
   readonly dialog = inject(MatDialog);
+  firestore: Firestore = inject(Firestore);
+
+  contacts: ContactInterface[] = [];
+  unsubContacts = this.subContacts();
 
   openDialog() {
     this.dialog.open(AddUserComponent);
+  }
+
+  subContacts() {
+
+    // return onSnapshot(collection(this.firestore, 'contacts'), (contacts) => {
+    //   this.contacts = [];
+    //   contacts.forEach(contact => {
+    //     this.contacts.push(contact.data());
+    //     this.contacts.forEach((contact) => console.log(contact));
+
+    //   });
+    // });
+
+    return onSnapshot(collection(this.firestore, 'contacts'), (contacts) => {
+      contacts.forEach(contact => {
+        this.contacts.push(this.contactObject(contact.data()));
+      });
+    });
+
+  }
+
+  contactObject(obj: any): ContactInterface {
+    return {
+      'name': {
+        first: obj.name.first,
+        last: obj.name.last
+      },
+      mail: obj.mail,
+      birthdate: obj.birthdate,
+      address: {
+        street: obj.address.street,
+        zip: obj.address.zip,
+        city: obj.address.city
+      }
+    }
   }
 
 }
