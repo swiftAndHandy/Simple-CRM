5015a9bc71267a66bb56d53f3ad782a4dc59d2da
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-09-27 11:47:24 +0200

    added an environment for knowledge reasons of hidding information on git

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment.development';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"simplecrm-5afda","appId":"1:370384440059:web:8a8561538a0e5dd4983e5e","storageBucket":"simplecrm-5afda.appspot.com","apiKey":"AIzaSyBfF8ob256bkUSkQvLyxlj6Q4KkEzHa65E","authDomain":"simplecrm-5afda.firebaseapp.com","messagingSenderId":"370384440059"})), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideFirestore(() => getFirestore())]
 };
